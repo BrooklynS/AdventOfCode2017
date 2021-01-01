4006a9ac67e4a23b7b1e3f9cc402fdca6e67978f
BrooklynS <brooklyntstine@yahoo.com>
2021-01-01 12:54:16 -0800

    Add timer to problem5

--- problem05.py
+++ problem05.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 def runInput(filename : str):
     
@@ -8,7 +9,8 @@ def runInput(filename : str):
     jumplist = list(map(lambda line: int(line), lines))
     stepsTaken = 0
     index = 0
-    while(index < len(jumplist)):
+    target = len(jumplist)
+    while(index < target):
         stepsTaken += 1
         jump = jumplist[index]
         jumplist[index] += 1
@@ -19,13 +21,15 @@ def runInput(filename : str):
     jumplist = list(map(lambda line: int(line), lines))
     stepsTaken = 0
     index = 0
-    while(index < len(jumplist)):
+    target = len(jumplist)
+    while(index < target):
         stepsTaken += 1
         jump = jumplist[index]
-        if(jumplist[index] >= 3):
+        if(jump >= 3):
             jumplist[index] -= 1
         else:
             jumplist[index] += 1
+
         index += jump
 
     print(f'Part2: {stepsTaken}')
@@ -33,9 +37,16 @@ def runInput(filename : str):
 
 def main():
     print("=========Test==========")
+    time1 = time.perf_counter()
     runInput("inputs/problem05test.txt")
+    time2 = time.perf_counter()
+    print(f'Elapsed: {(time2 - time1)*1000:0.3f} ms')
+
     print("=========Final==========")
+    time1 = time.perf_counter()
     runInput("inputs/problem05input.txt")
+    time2 = time.perf_counter()
+    print(f'Elapsed: {(time2 - time1)*1000:0.3f} ms')
 
 if __name__ == '__main__':
     main()
